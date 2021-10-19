3483381dbd12ddd2c5d452bb580358dd6d303b96
RodrigoPaulaLopes <rodrigolopesti1@gmail.com>
2021-10-19 16:02:58 -0300

    Icone da barra do navegador

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./public/css/style.css">
     <script src="https://kit.fontawesome.com/b25e671b20.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="shortcut icon" type="imagex/png" href="./public/img/abra-o-livro.png">
     <title>Rodrigo Lopes - Portfólio</title>
 </head>
 
